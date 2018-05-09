52def3e9a2309ff3ac69e6eb87669a0ca118e20a
dauce <dauce@port-dauce.ec-m.fr>
2018-05-09 12:42:52 +0200

    ajout modele backbone

---
+++ foveated-iclr/backbone_CNN_parts_def.py
@@ -0,0 +1,128 @@
+class Obj:
+    pass
+
+params = Obj()
+params.batch_size = 100
+
+batch = mnist.train.next_batch(params.batch_size)
+wave_tensor = wave_tensor_data(batch[0])
+
+DEPTH_WAV = 3
+
+DIM_5 = 16
+WIDTH = 2
+
+DEPTH_4 = 32
+DIM_4 = DIM_5 / WIDTH # 8
+
+DEPTH_3 = 64
+DIM_3 = DIM_4 / WIDTH # 4
+
+DEPTH_2 = 128
+DIM_2 = DIM_3 / WIDTH # 2
+
+DEPTH_1 = 256
+DIM_1 = DIM_2 / WIDTH # 1
+
+DIM_HIDDEN = 512
+
+NB_LABEL = 10
+
+STD = 1.
+
+nb_param_54 = (DEPTH_WAV * WIDTH * WIDTH) * DEPTH_4
+nb_param_43 = (DEPTH_4 * WIDTH * WIDTH) * DEPTH_3
+nb_param_32 = (DEPTH_3 * WIDTH * WIDTH) * DEPTH_2
+nb_param_21 = (DEPTH_2 * WIDTH * WIDTH) * DEPTH_1
+nb_param_1h = (DEPTH_1 + DEPTH_WAV + 1) * DIM_HIDDEN
+nb_param_hr = DIM_HIDDEN * NB_LABEL
+
+def weight_variable(shape, stddev = 0.1, name = "dummy"):
+    #initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.random_normal(shape, stddev = stddev)
+    #initial = tf.zeros(shape)
+    return tf.Variable(initial, name = name)
+
+x_5 = tf.placeholder(tf.float32, shape=[None, DIM_5, DIM_5, DEPTH_WAV])
+x_4 = tf.placeholder(tf.float32, shape=[None, DIM_4, DIM_4, DEPTH_WAV])
+x_3 = tf.placeholder(tf.float32, shape=[None, DIM_3, DIM_3, DEPTH_WAV])
+x_2 = tf.placeholder(tf.float32, shape=[None, DIM_2, DIM_2, DEPTH_WAV])
+x_1 = tf.placeholder(tf.float32, shape=[None, DIM_1, DIM_1, DEPTH_WAV])
+x_0 = tf.placeholder(tf.float32, shape=[None, 1, 1, 1])
+y = tf.placeholder(tf.float32, shape=[None, 10])
+keep_prob = tf.placeholder(tf.float32)
+batch_phase = tf.placeholder(tf.bool, name='bn_phase')
+
+l1_loss = tf.constant(0.0)
+
+# Paramètres 5 --> 4
+W_conv_54 = weight_variable([WIDTH, WIDTH, DEPTH_WAV, DEPTH_4], \
+                            stddev = STD / (WIDTH * WIDTH * DEPTH_WAV), \
+                            name = "W_conv_54")
+# Graph construction
+h_conv_4 = tf.nn.conv2d(x_5, W_conv_54, strides=[1, 2, 2, 1], padding='VALID', name='h_conv_4') 
+z_conv_4 = tf.nn.relu(h_conv_4)
+cat_conv_4 = tf.concat((z_conv_4, x_4), axis = 3)
+
+# Paramètres 4 --> 3
+W_conv_43 = weight_variable([WIDTH, WIDTH, DEPTH_4 + DEPTH_WAV, DEPTH_3],\
+                            stddev = STD / (WIDTH * WIDTH * (DEPTH_4 + DEPTH_WAV)), \
+                            name = "W_conv_43")
+
+# Graph construction
+h_conv_3 = tf.nn.conv2d(cat_conv_4, W_conv_43, strides=[1, 2, 2, 1], padding='VALID', name='h_conv_3') 
+z_conv_3 = tf.nn.relu(h_conv_3)
+cat_conv_3 = tf.concat((z_conv_3, x_3), axis = 3)
+
+# Paramètres 3 --> 2
+W_conv_32 = weight_variable([WIDTH, WIDTH, DEPTH_3 + DEPTH_WAV, DEPTH_2],\
+                            stddev = STD / (WIDTH * WIDTH * (DEPTH_3 + DEPTH_WAV)), \
+                            name = "W_conv_32")
+
+# Graph construction
+h_conv_2 = tf.nn.conv2d(cat_conv_3, W_conv_32, strides=[1, 2, 2, 1], padding='VALID', name='h_conv_2') 
+z_conv_2 = tf.nn.relu(h_conv_2)
+cat_conv_2 = tf.concat((z_conv_2, x_2), axis = 3)
+
+# Paramètres 2 --> 1
+W_conv_21 = weight_variable([WIDTH, WIDTH, DEPTH_2 + DEPTH_WAV, DEPTH_1],\
+                            stddev = STD / (WIDTH * WIDTH * (DEPTH_2 + DEPTH_WAV)), \
+                            name = "W_conv_21")
+
+# Graph construction
+h_conv_1 = tf.nn.conv2d(cat_conv_2, W_conv_21, strides=[1, 2, 2, 1], padding='VALID', name='h_conv_1') 
+z_conv_1 = tf.nn.relu(h_conv_1)
+cat_conv_1 = tf.concat((z_conv_1, x_1, x_0), axis = 3)
+z_flat1 = tf.reshape(cat_conv_1, [-1, DEPTH_1 + DEPTH_WAV + 1])
+
+# Hidden FC layer
+W_hidden = weight_variable([DEPTH_1 + DEPTH_WAV + 1, DIM_HIDDEN], stddev = STD / (DEPTH_1 + DEPTH_WAV + 1), name = "W_hidden")
+h_hidden = tf.matmul(z_flat1, W_hidden)
+z_hidden = tf.nn.relu(h_hidden)
+z_hidden_drop = tf.nn.dropout(z_hidden, keep_prob)
+
+# Read out Layer
+W_readout = weight_variable([DIM_HIDDEN, NB_LABEL], stddev = STD / DIM_HIDDEN, name = "W_readout")
+y_hat_logit = tf.matmul(z_hidden_drop, W_readout)
+
+# Loss
+classif_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_hat_logit))
+#l1_regularizer = tf.contrib.layers.l1_regularizer(
+#   scale=0.005, scope=None
+#)
+#weights = tf.trainable_variables() # all vars of your graph
+#regularization_penalty = tf.contrib.layers.apply_regularization(l1_regularizer, weights)
+regularized_loss = classif_loss #+ regularization_penalty # this loss needs to be min
+
+# Train
+train = tf.train.AdamOptimizer(1e-4).minimize(regularized_loss)
+
+# Eval
+correct_prediction = tf.equal(tf.argmax(y_hat_logit, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+
+
